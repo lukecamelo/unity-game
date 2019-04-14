0a28aa7262095e5758904d64581ae72270b429dd
Luke Camelo <azathothbeats@gmail.com>
2019-04-13 22:49:35 -0400

    Initial commit (a bit late)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+  public Transform target;
+  public float offsetDistance;
+  public float height;
+  private Transform _myTransform;
+
+  void Start()
+  {
+    if (target == null)
+      Debug.LogWarning("We do not have a target.");
+    _myTransform = transform;
+  }
+
+  void LateUpdate()
+  {
+
+    _myTransform.position = new Vector3(
+      target.position.x,
+      target.position.y + height,
+      target.position.z - offsetDistance
+    );
+
+    _myTransform.LookAt(target);
+  }
+}
